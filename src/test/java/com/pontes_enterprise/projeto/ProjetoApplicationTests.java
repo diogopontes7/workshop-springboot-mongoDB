38bee9366c53aeb467f9b52ee243bc10c6a0a5e2
diogopontes7 <diogopontespereira@gmail.com>
2023-08-28 16:52:06 +0100

    first commit

---
+++ src/test/java/com/pontes_enterprise/projeto/ProjetoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pontes_enterprise.projeto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
