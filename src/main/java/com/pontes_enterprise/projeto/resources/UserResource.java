3b97f22de2f162e070cb5fd4c938fb50388b07fc
diogopontes7 <diogopontespereira@gmail.com>
2023-08-30 23:13:00 +0100

    Metodos_insercao_Update_Delete

--- src/main/java/com/pontes_enterprise/projeto/resources/UserResource.java
+++ src/main/java/com/pontes_enterprise/projeto/resources/UserResource.java
@@ -2,16 +2,18 @@ package com.pontes_enterprise.projeto.resources;
 //O pacote chama se recurso,já que é o termo que usamos para referenciar os recursos REST 
 
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.pontes_enterprise.projeto.domain.User;
 import com.pontes_enterprise.projeto.services.UserService;
@@ -40,4 +42,25 @@ public class UserResource {
         return ResponseEntity.ok().body(new UserDTO(user));//Qual vai ser o copro da resposta, vai ser o usuario
     }
 
+    @RequestMapping( method = RequestMethod.POST)//Para saber o metodo,como pedimos informacao é o metodo GET do http, ou basta fazer getmapping
+    public ResponseEntity<Void> insert(@RequestBody UserDTO objDto){//Só para testar
+        User user = service.fromUserDTO(objDto);
+        user = service.insert(user);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();//Vamos retornar um cabecalho uri do recurso crado
+        return ResponseEntity.created(uri).build();//Retorna o codigo 201 quando criamos um novo recurso, retorna uma resposta vazia 201 com o cabecalho com a localizacao do recurso criado
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)//Para saber o metodo,como pedimos informacao é o metodo GET do http, ou basta fazer getmapping
+    public ResponseEntity<Void> delete(@PathVariable String id){//Só para testar
+        service.delete(id);
+        return ResponseEntity.noContent().build();//Para delete
+    }
+
+     @RequestMapping(value = "/{id}" ,  method = RequestMethod.PUT)//Para saber o metodo,como pedimos informacao é o metodo GET do http, ou basta fazer getmapping
+    public ResponseEntity<Void> update(@RequestBody UserDTO objDto, @PathVariable String id){//Só para testar
+        User user = service.fromUserDTO(objDto);
+        user.setId(id);//O userDTO vai ser o user com o id especificado
+        user = service.update(user);
+        return ResponseEntity.noContent().build();
+    }
 }
--- src/main/java/com/pontes_enterprise/projeto/services/UserService.java
+++ src/main/java/com/pontes_enterprise/projeto/services/UserService.java
@@ -10,6 +10,8 @@ import com.pontes_enterprise.projeto.domain.User;
 import com.pontes_enterprise.projeto.repository.UserRepository;
 import com.pontes_enterprise.projeto.services.exception.ObjectNotFoundException;
 
+import dto.UserDTO;
+
 @Service
 public class UserService {
 
@@ -25,5 +27,31 @@ public class UserService {
     public User findById(String id){
         Optional<User> user = repository.findById(id);
         return user.orElseThrow(() -> new ObjectNotFoundException("Objeto nao encontrado"));//Se nao encontrar usuario, lanca a excecao
+    }
+
+    public User insert(User obj){
+        return repository.insert(obj);//Ja esta implementado pelo MongoRepository
+    } 
+
+    //De UserDTo para user
+    public User fromUserDTO(UserDTO obj){
+        return new User(obj.getId(), obj.getName(), obj.getEmail());
+    }
+
+    public void delete(String id){
+        findById(id);
+        repository.deleteById(id);
+    }
+
+    public User update(User obj){//Dados que vao usar como requisicao
+        //Vamos procurar o objeto na base de dados, e dps com os dados metidos, atualizamos
+        User newObj = repository.findById(obj.getId()).orElseThrow(() -> new ObjectNotFoundException("Objeto nao encontrado"));
+        updateData(newObj, obj);//Copia os dados do obj ao newObj
+        return repository.save(newObj);
+    }
+
+    private void updateData(User newObj, User obj) {
+        newObj.setName(obj.getName());
+        newObj.setEmail(obj.getEmail());
     }
 }
